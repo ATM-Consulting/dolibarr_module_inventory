3865353263af9e4ab7c037f6a02cff975fdaab35
phf <phf@atm-consulting.fr>
2015-06-22 10:59:38 +0200

    FIX accents

--- inventory.php
+++ inventory.php
@@ -438,7 +438,7 @@ function generateODT(&$PDOdb, &$db, &$conf, &$langs, &$inventory)
 			,'date_maj'=>$inventory->get_date('date_maj', 'd/m/Y H:i')
 			,'numero'=>$inventory->getId()
 			,'warehouse'=>$warehouse->libelle
-			,'status'=>($inventory->status ? $langs->trans('inventoryValidate') : $langs->trans('inventoryDraft'))
+			,'status'=>($inventory->status ? $langs->transnoentities('inventoryValidate') : $langs->transnoentities('inventoryDraft'))
 			,'logo'=>DOL_DATA_ROOT."/mycompany/logos/".MAIN_INFO_SOCIETE_LOGO
 		)
 		,array()
