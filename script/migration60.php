e5014c5af5a00fa342ae4f8d2fbf779b3de70db6
atm-ph <phf@atm-consulting.Fr>
2017-09-11 15:12:17 +0200

    Fix la migration d'un dolibarr 5.0 vers 6.0 pose problème car des colonnes n'exists pas

---
+++ script/migration60.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if(!defined('INC_FROM_DOLIBARR')) {
+	define('INC_FROM_CRON_SCRIPT', true);
+
+	require('../config.php');
+
+}
+
+
+global $db;
+
+
+
+$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventory ADD COLUMN tms timestamp');
+$db->query('UPDATE '.MAIN_DB_PREFIX.'inventory SET tms = date_maj WHERE tms IS NULL');
+
+$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventorydet ADD COLUMN tms timestamp');
+$db->query('UPDATE '.MAIN_DB_PREFIX.'inventorydet SET tms = date_maj WHERE tms IS NULL');
+
+$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventorydet ADD COLUMN datec datetime DEFAULT NULL');
+$db->query('UPDATE '.MAIN_DB_PREFIX.'inventorydet SET datec = date_cre WHERE datec IS NULL');
