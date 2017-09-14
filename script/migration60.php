7adf4e2d66542338d762ed63834490919441d7b5
atm-ph <phf@atm-consulting.Fr>
2017-09-14 10:01:35 +0200

    Update simplification des requetes sql

--- script/migration60.php
+++ script/migration60.php
@@ -16,13 +16,8 @@ if(!defined('INC_FROM_DOLIBARR')) {
 
 global $db;
 
+$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventory CHANGE COLUMN date_maj tms timestamp');
 
+$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventorydet CHANGE COLUMN date_maj tms timestamp');
 
-$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventory ADD COLUMN tms timestamp');
-$db->query('UPDATE '.MAIN_DB_PREFIX.'inventory SET tms = date_maj WHERE tms IS NULL');
-
-$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventorydet ADD COLUMN tms timestamp');
-$db->query('UPDATE '.MAIN_DB_PREFIX.'inventorydet SET tms = date_maj WHERE tms IS NULL');
-
-$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventorydet ADD COLUMN datec datetime DEFAULT NULL');
-$db->query('UPDATE '.MAIN_DB_PREFIX.'inventorydet SET datec = date_cre WHERE datec IS NULL');
+$db->query('ALTER TABLE '.MAIN_DB_PREFIX.'inventorydet CHANGE COLUMN date_cre datec datetime DEFAULT NULL');
